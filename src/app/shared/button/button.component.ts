2b5583776b45593717eb51a7fa7d34218d0a316e
martyns0n <zogacc@gmail.com>
2019-06-30 16:06:52 +0300

    add styling and states for button component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   constructor() { }
+
+  @Input() disabled: boolean;
+  @Input() pseudo: boolean;
 
   ngOnInit() {
   }
