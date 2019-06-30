cf7380d6a10ff9101aae195613b81f03098eb8eb
martyns0n <zogacc@gmail.com>
2019-06-30 16:04:47 +0300

    add pages and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { PageLayoutModule } from './page-layout/page-layout.module';
+import { UiKitModule } from './ui-kit/ui-kit.module';
+import { ProfileModule } from './profile/profile.module';
 import { AppComponent } from './app.component';
+import { UiKitComponent } from './ui-kit/ui-kit.component';
+import { ProfileComponent } from './profile/profile.component';
 
+
+const appRoutes = [
+  // { path: '', component: AppComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'ui-kit', component: UiKitComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    PageLayoutModule
-    // RouterModule.forRoot([
-    //   { path: '', component: AppComponent }
-    // ])
+    PageLayoutModule,
+    UiKitModule,
+    ProfileModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
