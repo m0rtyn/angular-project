e9a2d7e6e1354b0cb086b166719962b59a3d5e05
martyns0n <zogacc@gmail.com>
2019-07-02 19:40:50 +0300

    add dropdown component

--- src/app/shared/dropdown/dropdown.component.ts
+++ src/app/shared/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent implements OnInit {
+
+  @Input() placeholder = '';
+  @Input() error = false;
+  @Input() disabled = false;
 
   constructor() { }
 
--- src/app/ui-kit/ui-kit.module.ts
+++ src/app/ui-kit/ui-kit.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UiKitComponent } from './ui-kit.component';
 import { ButtonComponent } from '../shared/button/button.component';
 import { InputComponent } from '../shared/input/input.component';
+import { DropdownComponent } from '../shared/dropdown/dropdown.component';
 
 
 
 @NgModule({
-  declarations: [UiKitComponent, ButtonComponent, InputComponent],
+  declarations: [UiKitComponent, ButtonComponent, InputComponent, DropdownComponent],
   imports: [
     CommonModule
   ]
