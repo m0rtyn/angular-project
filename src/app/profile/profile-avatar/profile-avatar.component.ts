63812f7d1858caa173810677bb767283ec59449c
martyns0n <zogacc@gmail.com>
2019-07-05 17:32:41 +0300

    add profile avatar component for profile page

--- src/app/profile/profile-avatar/profile-avatar.component.ts
+++ src/app/profile/profile-avatar/profile-avatar.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-profile-avatar',
+  selector: 'profile-avatar',
   templateUrl: './profile-avatar.component.html',
   styleUrls: ['./profile-avatar.component.scss']
 })
 export class ProfileAvatarComponent implements OnInit {
+
+  @Input() size = 'small';
 
   constructor() { }
 
