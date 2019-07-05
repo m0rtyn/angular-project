ecf6c97b9508c0de34a8a4d9c69775ce4942f10f
martyns0n <zogacc@gmail.com>
2019-07-05 17:32:12 +0300

    add form components, sections and styles for profile page

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiKitModule} from '../ui-kit/ui-kit.module';
 import { ProfileComponent } from './profile.component';
 import { ProfileAvatarComponent } from './profile-avatar/profile-avatar.component';
 import { ProfileEditDetailsComponent } from './profile-edit-details/profile-edit-details.component';
 import { ProfileWorkspaceSettingsComponent } from './profile-workspace-settings/profile-workspace-settings.component';
 
 @NgModule({
-  declarations: [ProfileComponent, ProfileAvatarComponent, ProfileEditDetailsComponent, ProfileWorkspaceSettingsComponent],
+  declarations: [
+    ProfileComponent,
+    ProfileAvatarComponent,
+    ProfileEditDetailsComponent,
+    ProfileWorkspaceSettingsComponent,
+    ProfileAvatarComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    UiKitModule
   ]
 })
 export class ProfileModule { }
